5077f20b3cefacbfe7c5a648b4d63cb82f9c1079
xrz-cloud <62951481+xrz-cloud@users.noreply.github.com>
2024-08-16 23:11:19 +0800

    [chore] remove useless code

--- docs/timeline/s.vue
+++ docs/timeline/s.vue
@@ -1,16 +1,12 @@
 <script setup>
 // import { ref } from "vue";
 import { data as ani } from "./toml.data.ts";
-import typeTrans from "../utils/typeTrans.ts";
 import validName from "../utils/get-valid-name.ts";
-import ssRealGen from "../utils/ss-real-gen.vue";
 import typeCodeGen from "../utils/type-code-gen.vue";
-import ffmpegCommandGen from "../utils/ffmpeg-command-gen.vue";
 import Link from "vitepress/dist/client/theme-default/components/VPLink.vue";
 import HA from "../utils/head-anchor.vue";
 import { ref } from "vue";
 import { useData } from "vitepress";
-// import TimeFormat from "hh-mm-ss";
 
 defineProps(["set_time", "min"]);
 const { isDark } = useData();
@@ -20,95 +16,13 @@ const isASL = (set_time) =>
   set_time === "ASL" ||
   set_time === "AutoSyncLatest";
 
-/**
- * 获取视频源
- * @param {string} source
- */
-function genSource(source) {
-  if (!source) return "未知";
-  const host = new URL(source).hostname;
-  if (host.match("b23") || host.match("bili")) return "B站";
-  else if (host.match("iqy")) return "爱奇艺";
-  else if (host.match("qq.com")) return "腾讯";
-  else return source;
-}
-
-/**
- * Clip模板
- * @param {string} clip
- * @param {string} time
- * @param {string} title
- * @param {string} ep
- * @param {string} ss
- */
-function genClip(clip, time, title, ep, ss) {
-  if (!clip) return "#";
-  else
-    return clip.replaceAll(
-      "TP-main::",
-      `https://alist.xrzyun.eu.org/aniclip-upload/${time}/${title}/${ep}/${validName(
-        `${ep}-${ss}`
-      )}.mp4`
-    );
-}
-
-let dts = {};
-let derr = {};
-// function ssGen_dts(id, ss, t, type) {
-//   try {
-//     if (!t) t = 0;
-//     else if (!Number(t)) t = TimeFormat.toS(t);
-//     if (!dts[id]) dts[id] = 0;
-//     if (!derr[id]) derr[id] = false;
-//     const t_ret = TimeFormat.fromS(TimeFormat.toS(ss) + dts[id]);
-//     if (type.match("_l") || type.match("丢失")) dts[id] += Number(t);
-//     return t_ret + (derr[id] ? "(可能有误)" : "");
-//   } catch (error) {
-//     derr[id] = true;
-//     console.error(error);
-//   }
-//   return "非法格式，此行数据可能有误";
-// }
-
-let dts2 = {};
-let derr2 = {};
-
 const show_watch = ref(true);
 const show_contribute = ref(true);
-// const count = ref(0);
 </script>
 
 <template>
   <div class="flex flex-col" :data-theme="isDark ? 'dark' : 'light'">
-    <div class="ds-form-control">
-      <label class="label cursor-pointer">
-        <span class="label-text">显示<code>观看删减片段</code></span>
-        <input
-          type="checkbox"
-          class="ds-toggle ds-toggle-accent"
-          :checked="show_watch"
-          :disabled="show_contribute"
-          @click="show_watch = !show_watch"
-        />
-        <span class="label-text"><--点击此处(已锁定)</span>
-      </label>
-    </div>
-    <div class="ds-form-control">
-      <label class="label cursor-pointer">
-        <span class="label-text">显示<code>贡献片段截取(FFMpeg)</code></span>
-        <input
-          type="checkbox"
-          class="ds-toggle ds-toggle-accent"
-          :checked="show_contribute"
-          :disabled="show_contribute"
-          @click="show_contribute = !show_contribute"
-        />
-        <span class="label-text"><--点击此处(已锁定)</span>
-      </label>
-    </div>
   </div>
-  <!-- <button @click="count++">You clicked me {{ count }} times.</button>
-  st:{{ set_time }} t:{{ time }} -->
   <div v-for="(ss, time) in ani.data" :key="time">
     <div v-if="!set_time || time === set_time">
       <HA :name="time" :id="time" lv="1">{{ time }}</HA>
